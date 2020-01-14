bcb24e410814e2957116be0658a6c156fd17517d
Keith Manning <kmanning@users.noreply.github.com>
2020-01-14 13:20:59 -0500

    Update Notes for creating future TerraformPluginVersions

--- src/TerraformPlugin.groovy
+++ src/TerraformPlugin.groovy
@@ -1,20 +1,12 @@
 /**
- * Development Notes
+ * Development Notes For Support version-specific workflows
  *
- * 1. Add the correct plugin interfaces (e.g. `TerraformValidateCommandPlugin`).
- * 2. Add the correct method if not arleady present.  If you do, ensure that you
- *    call `detectVersion()` at the beginning of the method.
- * 3. Introduce logic to the method that utilizes the `Comparable` class,
- *   `SemanticVersion` to determine what behavior.  The original break with
- *   the removal of the `-check-variables=false` in the
- *   `apply(TerraformValidateCommand)` provides a working example.
- * 4. If you introduced a new plugin interface, ensure that you add the
- *   the plugin class to the appropriate stage or command's `DEFAULT_PLUGINS`
- *   list.  See `TerraformValidateCommand` for an example.
- * 5. Finally, update the tests in `TerraformPluginTests` to cover the new
- *   behavior.
+ * 1. Create a new class that extends TerraformPluginVersion
+ * 2. Implement `apply` methods for any TerraformCommand and TerraformStage necessary
+ * 3. Modify `strategyFor`, to return your new TerraformPluginVersion, for which
+ *    ever set of terraform-versions that require your new workflow changes.
  *
- * See apply(TerraformValidateCommand) for an over-documented example
+ * See TerraformPluginVersion11 or TerraformPluginVersion12 for an example
  * before strating on your own.
  */
 class TerraformPlugin implements TerraformValidateCommandPlugin, TerraformValidateStagePlugin {
@@ -68,16 +60,7 @@ class TerraformPlugin implements TerraformValidateCommandPlugin, TerraformValida
 
     @Override
     void apply(TerraformValidateCommand command) {
-        // This must be at the top of every apply() call.  This is
-        // the best way to ensure that Jenkinsfile.instance.original is
-        // properly set so that we can detect the terraform-version.
         def version = detectVersion()
-
-        // Due to CPS shenanigans you can't use Groovy magic and do
-        // version < new SemanticVersion('1.2.3').  This becomes
-        // if(-1) or if(0) or if(1) resulting in truthy values for
-        // both less than and greater than and false-ey values when
-        // equal.
 
         def strategy = strategyFor(version.version)
         strategy.apply(command)
