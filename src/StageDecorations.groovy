5ae2aa044addfdfadcfcf9e0e06dc2af18710c24
Keith Manning <kmanning@users.noreply.github.com>
2020-03-18 13:54:46 -0400

    Add delegation so plugin DSL commands route properly

--- src/StageDecorations.groovy
+++ src/StageDecorations.groovy
@@ -2,19 +2,18 @@ class StageDecorations {
     private Map<String,Closure> decorations = [:]
 
     public apply(String group = null, Closure innerClosure) {
-        def currentDecorations = decorations.get(group)
-
-        if (currentDecorations != null) {
-            currentDecorations(innerClosure)
-        } else {
-            innerClosure()
-        }
+        def currentDecorations = decorations.get(group) ?: { stage -> stage() }
+        currentDecorations.delegate = innerClosure.owner
+        currentDecorations(innerClosure)
     }
 
     public add(String group = null, Closure decoration) {
         def existingDecoration = decorations.get(group) ?: { stage -> stage() }
         def newDecoration = { stage ->
+            decoration.delegate = delegate
             decoration() {
+                stage.delegate = delegate
+                existingDecoration.delegate = delegate
                 existingDecoration(stage)
             }
         }
