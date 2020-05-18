0dc096ff3d4d1074bbd0084261599ec55ae9ecc2
James Leopold <james.leopold@coxautoinc.com>
2020-05-18 13:40:50 -0400

    list installed gems

--- src/TerraformLandscapePlugin.groovy
+++ src/TerraformLandscapePlugin.groovy
@@ -7,7 +7,7 @@ class TerraformLandscapePlugin implements TerraformPlanCommandPlugin {
 
     @Override
     public void apply(TerraformPlanCommand command) {
-        command.withPrefix("gem install --no-document --version 0.2.2 terraform_landscape")
+        command.withPrefix("gem list installed | grep terraform_landscape")
         command.withSuffix(" | landscape")
     }
 
