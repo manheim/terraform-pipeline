562f287935018a39e2eda81f7746792ca4e31f4e
James Leopold <james.leopold@coxautoinc.com>
2020-06-04 15:36:34 -0400

    add create file

--- src/TerraformEnvironmentStage.groovy
+++ src/TerraformEnvironmentStage.groovy
@@ -196,6 +196,7 @@ class TerraformEnvironmentStage implements Stage {
         def data = JsonOutput.toJson([body: commentBody])
         //sh "echo ${data}"
         def bodyPath = new File("${tmpDir}/body.txt")
+        bodyPath.createNewFile()
         bodyPath.write(data)
         //writeFile(file: bodyPath, text: data)
         def url = "${apiBaseUrl}repos/${repoSlug}/issues/${issueNumber}/comments"
