a053b588b145162a88ba236a7f1a3d5888050c0a
James Leopold <james.leopold@coxautoinc.com>
2020-06-04 15:46:21 -0400

    wip

--- src/TerraformPlanResultsPR.groovy
+++ src/TerraformPlanResultsPR.groovy
@@ -70,10 +70,10 @@ class TerraformPlanResultsPR implements TerraformPlanCommandPlugin, TerraformEnv
 
                 echo "Creating comment in GitHub"
                 withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'man_releng', usernameVariable: 'FOO', passwordVariable: 'GITHUB_TOKEN']]) {
-                    def tmpDir = pwd(tmp: true)
-                    def bodyPath = "${tmpDir}/body.txt"
+                    //def tmpDir = pwd(tmp: true)
+                    def bodyPath = "body.txt"
                     writeFile(file: bodyPath, text: '')
-                    cmd = createGithubComment(prNum, commentBody, repoSlug, 'man_releng', "https://${repoHost}/api/v3/", tmpDir)
+                    cmd = createGithubComment(prNum, commentBody, repoSlug, 'man_releng', "https://${repoHost}/api/v3/")
                     output = sh(script: cmd, returnStdout: true).trim()
 
                     def headers = readFile('comment.headers').trim()
