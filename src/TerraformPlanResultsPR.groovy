6277926b5700823eae6df56e7561243a9f57bd46
James Leopold <james.leopold@coxautoinc.com>
2020-06-04 11:35:42 -0400

    test with echo

--- src/TerraformPlanResultsPR.groovy
+++ src/TerraformPlanResultsPR.groovy
@@ -42,7 +42,7 @@ class TerraformPlanResultsPR implements TerraformPlanCommandPlugin, TerraformEnv
 
     public static Closure addComment() {
         return { closure -> 
-            sh "echo HERE#########"
+            sh "echo BLAH"
             closure()
         }
     }
