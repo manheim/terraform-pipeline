a124841324a2d36f7a626404131467a5ea06dd1a
James Leopold <james.leopold@coxautoinc.com>
2020-06-04 12:03:32 -0400

    comment out planoutput

--- src/TerraformPlanResultsPR.groovy
+++ src/TerraformPlanResultsPR.groovy
@@ -56,7 +56,7 @@ class TerraformPlanResultsPR implements TerraformPlanCommandPlugin, TerraformEnv
                 def planOutput = ''
                 def planStderr = ''
 
-                planOutput = readFile('plan.out').replaceAll(/\u001b\[[0-9;]+m/, '').replace(/^\[[0-9;]+m/, '')
+                //planOutput = readFile('plan.out').replaceAll(/\u001b\[[0-9;]+m/, '').replace(/^\[[0-9;]+m/, '')
                 //if (fileExists('plan.err')) {
                 //    planStderr = readFile('plan.err').replaceAll(/\u001b\[[0-9;]+m/, '').replace(/^\[[0-9;]+m/, '').trim()
                 //}
