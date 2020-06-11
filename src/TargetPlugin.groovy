e786413906d5d5b7f7adf8de11ceeafb88fce5a0
James Leopold <james.leopold@coxautoinc.com>
2020-06-11 09:16:05 -0400

    work in progress

--- src/TargetPlugin.groovy
+++ src/TargetPlugin.groovy
@@ -1,13 +1,51 @@
-class TargetPlugin implements TerraformPlanCommandPlugin {
+import static TerraformEnvironmentStage.ALL
+
+class TargetPlugin implements TerraformPlanCommandPlugin, TerraformApplyCommandPlugin, TerraformEnvironmentStagePlugin {
     public static void init() {
         TargetPlugin plugin = new TargetPlugin()
 
         TerraformPlanCommand.addPlugin(plugin)
+        TerraformApplyCommand.addPlugin(plugin)
+        TerraformEnvironmentStage.addPlugin(plugin)
     }
 
     @Override
     public void apply(TerraformPlanCommand command) {
-        command.withSuffix("| landscape")
+        def targets = Jenkinsfile.instance.getEnv().RESOURCE_TARGETS
+        if("${targets}" != '' && targets != null) {
+            for(item in "${targets}".split(',')) {
+                command.withArgument("-target ${item}")
+            }
+        }
+    }
+
+    @Override
+    public void apply(TerraformApplyCommand command) {
+        def targets = Jenkinsfile.instance.getEnv().RESOURCE_TARGETS
+        if("${targets}" != '' && targets != null) {
+            for(item in "${targets}".split(',')) {
+                command.withArgument("-target ${item}")
+            }
+        }
+    }
+    
+    @Override
+    public void apply(TerraformEnvironmentStage stage) {
+        stage.decorate(ALL, addBuildParams())
+    }
+
+    public static Closure addBuildParams() {
+        return { closure ->
+            def params = [
+                string(name: 'RESOURCE_TARGETS', defaultValue: '', description: 'comma-separated list of resource addresses to pass to plan and apply "-target=" parameters'),
+            ]
+            def props = [
+                parameters(params)
+            ]
+            properties(props)
+
+            closure()
+        }
     }
 
 }
