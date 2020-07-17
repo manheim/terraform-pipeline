973c96ccf81137b0d9f0b4c24d7c587479c12400
James Leopold <james.leopold@coxautoinc.com>
2020-07-17 11:19:53 -0400

    add init() method

--- src/DestroyPlugin.groovy
+++ src/DestroyPlugin.groovy
@@ -3,6 +3,8 @@ class DestroyPlugin implements TerraformEnvironmentStagePlugin {
     DestroyPlugin() {
     }
 
+    public static void init() {}
+
     @Override
     public void apply(TerraformEnvironmentStage stage) {
         stage.withStrategy(new DestroyStrategy())
