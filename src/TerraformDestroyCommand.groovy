ee06537416ee5f7b0fef4ada60b17039f11807b7
James Leopold <james.leopold@coxautoinc.com>
2020-07-17 11:49:39 -0400

    testing destroy with extends

--- src/TerraformDestroyCommand.groovy
+++ src/TerraformDestroyCommand.groovy
@@ -1,8 +1,18 @@
 class TerraformDestroyCommand extends TerraformApplyCommand{
 
+    private String command
+    String environment
+
     public TerraformDestroyCommand(String environment) {
         this.environment = environment
         this.command = "destroy"
     }
-    
+
+    @Override
+    public static TerraformDestroyCommand instanceFor(String environment) {
+        return new TerraformDestroyCommand(environment)
+            .withInput(false)
+            .withArgument("-auto-approve")
+    }
+
 }
