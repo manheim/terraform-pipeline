d37bf3df1e1d7b8e7593a37d8a42d518058e40f0
James Leopold <james.leopold@coxautoinc.com>
2020-07-20 13:41:24 -0400

    change back to master

--- src/ConditionalApplyPlugin.groovy
+++ src/ConditionalApplyPlugin.groovy
@@ -7,7 +7,7 @@ public class ConditionalApplyPlugin implements TerraformEnvironmentStagePlugin {
     private String branch
 
     ConditionalApplyPlugin() {
-        branch = 'destroy_test' // TODO: CHANGE THIS BACK TO MASTER!
+        branch = 'master'
     }
 
     @Override
