57e21309e6f0ad12cdb55903651ed3cc54374630
James Leopold <james.leopold@coxautoinc.com>
2020-07-20 07:50:24 -0400

    change name of method

--- src/DestroyPlugin.groovy
+++ src/DestroyPlugin.groovy
@@ -3,7 +3,7 @@ class DestroyPlugin implements TerraformEnvironmentStagePlugin {
     public static void init() {
         DestroyPlugin plugin = new DestroyPlugin()
 
-        ConfirmApplyPlugin.withConfirmationMessage("YOU ARE ABOUT TO DESTROY YOUR ENVIRONMENT")
+        ConfirmApplyPlugin.withConfirmMessage("YOU ARE ABOUT TO DESTROY YOUR ENVIRONMENT")
         ConfirmApplyPlugin.withOkMessage("!!!DESTROY DESTROY DESTROY!!!")
         TerraformPlanCommand.withArgument('-destroy')
         TerraformApplyCommand.withCommand('destroy')
