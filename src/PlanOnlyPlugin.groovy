e815dfc9a1010351bcbefd441c34e9f1c9347a39
James Leopold <james.leopold@coxautoinc.com>
2020-07-29 12:36:41 -0400

    removing changes for fail plan on changes

--- src/PlanOnlyPlugin.groovy
+++ src/PlanOnlyPlugin.groovy
@@ -1,31 +1,19 @@
-class PlanOnlyPlugin implements TerraformPlanCommandPlugin, TerraformEnvironmentStagePlugin {
+class PlanOnlyPlugin implements TerraformEnvironmentStagePlugin {
 
     public static void init() {
         PlanOnlyPlugin plugin = new PlanOnlyPlugin()
 
-        TerraformPlanCommand.addPlugin(plugin)
         TerraformEnvironmentStage.addPlugin(plugin)
-        TerraformEnvironmentStage.addParam([
-            $class: 'hudson.model.BooleanParameterDefinition',
-            name: "FAIL_PLAN_ON_CHANGES",
-            defaultValue: false,
-            description: 'Plan run with -detailed-exitcode; ANY CHANGES will cause failure'
-        ])
     }
 
-    @Override
-    public void apply(TerraformPlanCommand command) {
-        // Jenkins boolen env vars get converted to strings
-        if (Jenkinsfile.instance.getEnv().FAIL_PLAN_ON_CHANGES == 'true') {
-            // When we have 'terraform plan -detailed-exitcode | landscape'
-            // the return code will always be 0 unless we set this prefix
-            command.withPrefix('set -o pipefail;')
-            command.withArgument('-detailed-exitcode')
+    public Closure skipApply() {
+        return  { closure ->
+            echo "Skipping apply stage. PlanOnlyPlugin is enabled."
         }
     }
 
     @Override
     public void apply(TerraformEnvironmentStage stage) {
-        stage.withStrategy(new PlanOnlyStrategy())
+        stage.decorateAround(APPLY, skipApply())
     }
 }
