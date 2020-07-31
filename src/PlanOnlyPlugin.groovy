ee8a9749327b833ff90fb32e9a43f7c03c00a084
James Leopold <james.leopold@coxautoinc.com>
2020-07-31 10:33:12 -0400

    test fail on changes

--- src/PlanOnlyPlugin.groovy
+++ src/PlanOnlyPlugin.groovy
@@ -1,11 +1,12 @@
 import static TerraformEnvironmentStage.APPLY
 import static TerraformEnvironmentStage.CONFIRM
 
-class PlanOnlyPlugin implements TerraformEnvironmentStagePlugin {
+class PlanOnlyPlugin implements TerraformEnvironmentStagePlugin, TerraformPlanCommandPlugin {
 
     public static void init() {
         PlanOnlyPlugin plugin = new PlanOnlyPlugin()
 
+        TerraformPlanCommand.addPlugin(plugin)
         TerraformEnvironmentStage.addPlugin(plugin)
     }
 
@@ -19,5 +20,11 @@ class PlanOnlyPlugin implements TerraformEnvironmentStagePlugin {
     public void apply(TerraformEnvironmentStage stage) {
         stage.decorateAround(CONFIRM, skipStage(CONFIRM))
         stage.decorateAround(APPLY, skipStage(APPLY))
+    }
+
+    @Override
+    public void apply(TerraformPlanCommand command) {
+        command.withPrefix('set -e; set -o pipefail;')
+        command.withArgument('-detailed-exitcode')
     }
 }
