b3faf851b81ebf80db59290538f4b2833efb8997
James Leopold <james.leopold@coxautoinc.com>
2020-09-01 15:46:03 -0400

    switch branch to master

--- src/ConditionalApplyPlugin.groovy
+++ src/ConditionalApplyPlugin.groovy
@@ -6,7 +6,7 @@ public class ConditionalApplyPlugin implements TerraformEnvironmentStagePlugin {
     private String branch
 
     ConditionalApplyPlugin() {
-        branch = 'PR-7'
+        branch = 'master'
     }
 
     @Override
