afdc7a0fbed602fcba8bb22714d7bfeae28bc90c
James Leopold <james.leopold@coxautoinc.com>
2020-09-01 15:43:00 -0400

    Test PR 7

--- src/ConditionalApplyPlugin.groovy
+++ src/ConditionalApplyPlugin.groovy
@@ -6,7 +6,7 @@ public class ConditionalApplyPlugin implements TerraformEnvironmentStagePlugin {
     private String branch
 
     ConditionalApplyPlugin() {
-        branch = 'master'
+        branch = 'PR-7'
     }
 
     @Override
