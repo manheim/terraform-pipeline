6b431ed1f91620c01fb6824bb64db847478e9ae1
Jason Antman <jason@jasonantman.com>
2020-12-17 11:45:34 -0500

    finish TerraformEnvironmentStageShellHookPlugin tests

--- test/TerraformEnvironmentStageShellHookPluginTest.groovy
+++ test/TerraformEnvironmentStageShellHookPluginTest.groovy
@@ -12,7 +12,7 @@ import static org.mockito.Mockito.times
 import static org.mockito.Mockito.verify
 import static org.mockito.Mockito.spy
 import static org.mockito.Mockito.doReturn
-import org.mockito.InOrder
+import static org.mockito.Mockito.verifyNoMoreInteractions
 
 import static TerraformEnvironmentStage.ALL
 import static TerraformEnvironmentStage.INIT_COMMAND
@@ -21,10 +21,244 @@ import static TerraformEnvironmentStage.PLAN_COMMAND
 import static TerraformEnvironmentStage.APPLY
 import static TerraformEnvironmentStage.APPLY_COMMAND
 
+import org.mockito.InOrder
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
 import de.bechte.junit.runners.context.HierarchicalContextRunner
+
+@RunWith(HierarchicalContextRunner.class)
+class HookPointTest {
+
+    public class Constructor {
+        @Test
+        void setsHookName() {
+            def hp = new HookPoint("foo")
+            assertEquals(hp.getName(), "foo")
+        }
+    }
+
+    public class IsConfigured {
+        @Test
+        void falseIfAllNull() {
+            def hp = new HookPoint("foo")
+            assertFalse(hp.isConfigured())
+        }
+
+        @Test
+        void trueIfRunBefore() {
+            def hp = new HookPoint("foo")
+            hp.runBefore = 'bar'
+            assertTrue(hp.isConfigured())
+        }
+
+        @Test
+        void trueIfRunAfterOnSuccess() {
+            def hp = new HookPoint("foo")
+            hp.runAfterOnSuccess = 'bar'
+            assertTrue(hp.isConfigured())
+        }
+
+        @Test
+        void trueIfRunAfterOnFailure() {
+            def hp = new HookPoint("foo")
+            hp.runAfterOnFailure = 'bar'
+            assertTrue(hp.isConfigured())
+        }
+
+        @Test
+        void trueIfRunAfterAlways() {
+            def hp = new HookPoint("foo")
+            hp.runAfterAlways = 'bar'
+            assertTrue(hp.isConfigured())
+        }
+    }
+
+    public class GetClosure {
+        @After
+        public void reset() {
+            Jenkinsfile.instance = null
+            TerraformEnvironmentStage.reset()
+            TerraformEnvironmentStageShellHookPlugin.reset()
+        }
+
+        @Test
+        void runsAllConfiguredHooks() {
+            def hp = new HookPoint("foo")
+            hp.runBefore = 'run-before'
+            hp.runAfterOnSuccess = 'after-success'
+            hp.runAfterOnFailure = 'after-failure'
+            hp.runAfterAlways = 'after-always'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { -> sh 'passedClosure' }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            result.call(passedClosure)
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('run-before')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-success')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-always')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        @SuppressWarnings('EmptyCatchBlock')
+        void runsHooksOnFailure() {
+            def hp = new HookPoint("foo")
+            hp.runBefore = 'run-before'
+            hp.runAfterOnSuccess = 'after-success'
+            hp.runAfterOnFailure = 'after-failure'
+            hp.runAfterAlways = 'after-always'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { ->
+                sh 'passedClosure'
+                throw new Exception()
+            }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+
+            try {
+                result.call(passedClosure)
+            } catch (Exception e) { }
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('run-before')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-failure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-always')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        void runsOnlyBeforeHook() {
+            def hp = new HookPoint("foo")
+            hp.runBefore = 'run-before'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { -> sh 'passedClosure' }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            result.call(passedClosure)
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('run-before')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        void runsOnlyAfterSuccessHook() {
+            def hp = new HookPoint("foo")
+            hp.runAfterOnSuccess = 'after-success'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { -> sh 'passedClosure' }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            result.call(passedClosure)
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-success')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        void runsOnlyAfterAlwaysHook() {
+            def hp = new HookPoint("foo")
+            hp.runAfterAlways = 'after-always'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { -> sh 'passedClosure' }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            result.call(passedClosure)
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-always')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        @SuppressWarnings('EmptyCatchBlock')
+        void runsOnlyAfterAlwaysHookOnFailure() {
+            def hp = new HookPoint("foo")
+            hp.runAfterAlways = 'after-always'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { ->
+                sh 'passedClosure'
+                throw new Exception()
+            }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            try {
+                result.call(passedClosure)
+            } catch (Exception e) { }
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-always')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+
+        @Test
+        @SuppressWarnings('EmptyCatchBlock')
+        void runsOnlyAfterFailureHook() {
+            def hp = new HookPoint("foo")
+            hp.runAfterOnFailure = 'after-failure'
+
+            def dummyJenkinsfile = mock(DummyJenkinsfile.class)
+
+            def passedClosure = { ->
+                sh 'passedClosure'
+                throw new Exception()
+            }
+            passedClosure.delegate = dummyJenkinsfile
+
+            def result = hp.getClosure()
+            result.delegate = dummyJenkinsfile
+            try {
+                result.call(passedClosure)
+            } catch (Exception e) { }
+
+            InOrder inOrder = inOrder(dummyJenkinsfile);
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('passedClosure')
+            inOrder.verify(dummyJenkinsfile, times(1)).sh('after-failure')
+            inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(dummyJenkinsfile)
+        }
+    }
+}
 
 @RunWith(HierarchicalContextRunner.class)
 class TerraformEnvironmentStageShellHookPluginTest {
@@ -214,6 +448,7 @@ class TerraformEnvironmentStageShellHookPluginTest {
             inOrder.verify(mockStage, times(1)).decorate(ALL, cAll)
             inOrder.verify(mockStage, times(1)).decorate(APPLY, cApply)
             inOrder.verifyNoMoreInteractions()
+            verifyNoMoreInteractions(mockStage)
         }
     }
 
