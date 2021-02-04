9e1f80c7e1cb8fbdfc6fe62ed18bf4fcee79a583
Keith Manning <kmanning@users.noreply.github.com>
2021-02-04 15:42:57 -0500

    Use ResetStaticStateExtension in AnsiColorPluginTest

--- test/AnsiColorPluginTest.groovy
+++ test/AnsiColorPluginTest.groovy
@@ -2,18 +2,14 @@ import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.hasItem
 import static org.hamcrest.Matchers.instanceOf
 
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(ResetStaticStateExtension.class)
 class AnsiColorPluginTest {
     @Nested
     public class Init {
-        @AfterEach
-        void resetPlugins() {
-            TerraformEnvironmentStage.reset()
-        }
-
         @Test
         void modifiesTerraformEnvironmentStage() {
             AnsiColorPlugin.init()
