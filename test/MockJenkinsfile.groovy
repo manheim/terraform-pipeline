6accd0f8712142d86375af1ebd571dd773b59721
Keith Manning <kmanning@users.noreply.github.com>
2021-02-08 10:36:14 -0500

    Allow MockJenkinsfile methods to be chained

--- test/MockJenkinsfile.groovy
+++ test/MockJenkinsfile.groovy
@@ -1,19 +1,37 @@
-import static org.mockito.Mockito.when
 import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.mockingDetails
+import static org.mockito.Mockito.when
 
 public class MockJenkinsfile {
 
-    public static withEnv(Map env) {
-        Jenkinsfile.instance = mock(Jenkinsfile.class)
+    public static withEnv(Map env = [:]) {
+        if (needsJenkinsfileInstanceMock()) {
+            mockJenkinsfileInstance()
+        }
+
         when(Jenkinsfile.instance.getEnv()).thenReturn(env)
+        return this
     }
 
     public static withMockedOriginal() {
         Jenkinsfile.original = new DummyJenkinsfile()
+        return this
     }
 
     public static withStandardizedRepoSlug(String slug) {
-        Jenkinsfile.instance = mock(Jenkinsfile.class)
+        if (needsJenkinsfileInstanceMock()) {
+            mockJenkinsfileInstance()
+        }
+
         when(Jenkinsfile.instance.getStandardizedRepoSlug()).thenReturn(slug)
+        return this
+    }
+
+    public static needsJenkinsfileInstanceMock() {
+        return Jenkinsfile.instance == null || !mockingDetails(Jenkinsfile.instance).isMock()
+    }
+
+    public static mockJenkinsfileInstance() {
+        Jenkinsfile.instance = mock(Jenkinsfile.class)
     }
 }
