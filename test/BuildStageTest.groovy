1296500793b167f6a8b85ea073a27fe43b1f894b
Keith Manning <kmanning@users.noreply.github.com>
2021-02-08 23:05:27 -0500

    Use MockJenkins in BuildStageTest

--- test/BuildStageTest.groovy
+++ test/BuildStageTest.groovy
@@ -1,7 +1,6 @@
 import static org.mockito.Mockito.spy
 import static org.mockito.Mockito.doReturn
 
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -9,16 +8,10 @@ class BuildStageTest {
 
     @Nested
     public class Build {
-        @AfterEach
-        void reset() {
-            Jenkinsfile.original = null
-        }
-
         @Test
         void buildsWithoutError() {
             def expectedPipelineConfig = { -> }
-            Jenkinsfile.original = new Expando()
-            Jenkinsfile.original.ApplyJenkinsfileClosure = { closure -> }
+            MockJenkinsfile.withMockedOriginal()
 
             BuildStage stage = spy(new BuildStage())
             doReturn(expectedPipelineConfig).when(stage).pipelineConfiguration()
