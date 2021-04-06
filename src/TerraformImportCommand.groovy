5cd0fbb55e398cefe8bd8870a47414dc6563aad9
Patrick Aikens <patrick.aikens@coxautoinc.com>
2021-04-06 12:47:58 -0400

    Remove pluggable interface in anticipation of #360 being merged

--- src/TerraformImportCommand.groovy
+++ src/TerraformImportCommand.groovy
@@ -1,7 +1,9 @@
-class TerraformImportCommand implements TerraformCommand, Pluggable<TerraformImportCommandPlugin> {
+class TerraformImportCommand implements TerraformCommand, Resettable {
     private String environment
     private static String resource
     private static String targetPath
+    private static plugins = []
+    private appliedPlugins = []
 
     TerraformImportCommand(String environment) {
         this.environment = environment
@@ -48,7 +50,46 @@ class TerraformImportCommand implements TerraformCommand, Pluggable<TerraformImp
     public static void reset() {
         this.resource = ''
         this.targetPath = ''
-        resetPlugins()
+        this.resetPlugins()
+    }
+
+    /**
+     * Assures that all plugins are applied, and are applied at most once. It
+     * can be safely called multiple times.
+     */
+    public applyPlugins() {
+        def remainingPlugins = plugins - appliedPlugins
+
+        for (TerraformImportCommandPlugin plugin in remainingPlugins) {
+            plugin.apply(this)
+            appliedPlugins << plugin
+        }
+    }
+
+    /**
+     * Accepts a plugin of the appropriate type and adds it to the list of plugins.
+     *
+     * @param plugin The plugin to add
+     */
+    public static void addPlugin(TerraformImportCommandPlugin plugin) {
+        plugins << plugin
+    }
+
+    public static void setPlugins(plugins) {
+        this.plugins = plugins
+    }
+
+    public static getPlugins() {
+        return plugins
+    }
+
+    /**
+     * Reset plugins will reset the plugin list to a default set of plugins.
+     *
+     * @param defaultPlugins list of plugins to set, default: []
+     */
+    public static void resetPlugins(defaultPlugins = []) {
+        this.plugins = defaultPlugins.clone()
     }
 
     public static instanceFor(String environment) {
