2cdc0cc9870f7eff6d6c8cdf478a7db048a516bd
James Leopold <james.leopold@coxautoinc.com>
2022-02-03 14:33:26 -0500

    import mock

--- test/PassPlanFilePluginTest.groovy
+++ test/PassPlanFilePluginTest.groovy
@@ -2,6 +2,7 @@ import static org.hamcrest.Matchers.containsString
 import static org.hamcrest.Matchers.hasItem
 import static org.hamcrest.Matchers.instanceOf
 import static org.hamcrest.MatcherAssert.assertThat
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.any;
